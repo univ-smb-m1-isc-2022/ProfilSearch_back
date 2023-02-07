686caf807e591981566fa8232d931656a2cce1d7
Louise <louiisebld@gmail.com>
2023-02-07 16:45:58 +0100

    init

---
+++ src/main/java/fr/louisetom/profilsearch/ProfilsearchApplication.java
@@ -0,0 +1,13 @@
+package fr.louisetom.profilsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfilsearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProfilsearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/louisetom/profilsearch/ProfilsearchApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.louisetom.profilsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfilsearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
