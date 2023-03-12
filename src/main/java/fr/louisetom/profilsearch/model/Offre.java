cdae05e7d9531b3cd3157837bc1273a842a0d195
Louise <72506187+louisebld@users.noreply.github.com>
2023-03-12 19:14:03 +0100

    feat: add many to many offre

--- src/main/java/fr/louisetom/profilsearch/model/Offre.java
+++ src/main/java/fr/louisetom/profilsearch/model/Offre.java
@@ -6,6 +6,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "Offre")
@@ -24,13 +26,22 @@ public class Offre {
     private String place;
     private int salary;
 
-    public Offre(String name, Date creation_date, String description, String type, String place, int salary) {
+    @ManyToMany
+    @JoinTable(
+            name = "question_offre",
+            joinColumns = @JoinColumn(name = "offre_id"),
+            inverseJoinColumns = @JoinColumn(name = "question_id")
+    )
+    private Set<Question> questions = new HashSet<>();
+
+    public Offre(String name, Date creation_date, String description, String type, String place, int salary, Set<Question> questions) {
         this.name = name;
         this.creation_date = creation_date;
         this.description = description;
         this.type = type;
         this.place = place;
         this.salary = salary;
+        this.questions = questions;
     }
 
 }
