a5913064e8410b341bfcb25c69713172b6dc915e
Louise <72506187+louisebld@users.noreply.github.com>
2023-03-12 19:13:09 +0100

    refacto: delete question_offre

--- src/main/java/fr/louisetom/profilsearch/repository/Question_OffreRepository.java
+++
@@ -1,7 +0,0 @@
-package fr.louisetom.profilsearch.repository;
-
-import fr.louisetom.profilsearch.model.Question_Offre;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface Question_OffreRepository extends JpaRepository<Question_Offre, Long> {
-}
