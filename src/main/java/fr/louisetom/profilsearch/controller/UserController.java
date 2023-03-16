bb928a2d6389d23829b2a0d08604c35055169f86
Louise <72506187+louisebld@users.noreply.github.com>
2023-03-16 17:10:39 +0100

    feat: user model repo & controller

---
+++ src/main/java/fr/louisetom/profilsearch/controller/UserController.java
@@ -0,0 +1,29 @@
+package fr.louisetom.profilsearch.controller;
+
+import fr.louisetom.profilsearch.exception.ResourceNotFoundException;
+import fr.louisetom.profilsearch.model.User;
+import fr.louisetom.profilsearch.repository.UserRepository;
+import fr.louisetom.profilsearch.security.CurrentUser;
+import fr.louisetom.profilsearch.security.UserPrincipal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000")
+@RequestMapping("/profilsearch/user")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/me")
+    @PreAuthorize("hasRole('USER')")
+    public User getCurrentUser(@CurrentUser UserPrincipal userPrincipal) {
+        return userRepository.findById(userPrincipal.getId())
+                .orElseThrow(() -> new ResourceNotFoundException("User", "id", userPrincipal.getId()));
+    }
+}
---
+++ src/main/java/fr/louisetom/profilsearch/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package fr.louisetom.profilsearch.repository;
+
+import fr.louisetom.profilsearch.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    Boolean existsByEmail(String email);
+
+}
