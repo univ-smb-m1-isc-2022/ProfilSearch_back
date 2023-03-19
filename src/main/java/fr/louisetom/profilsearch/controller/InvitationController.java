10260e60da38bf472b7cda1f23f7f9b9dc43c29a
Louise <72506187+louisebld@users.noreply.github.com>
2023-03-19 17:01:14 +0100

    feat: send mail invitation

--- src/main/java/fr/louisetom/profilsearch/controller/InvitationController.java
+++ src/main/java/fr/louisetom/profilsearch/controller/InvitationController.java
@@ -1,11 +1,15 @@
 package fr.louisetom.profilsearch.controller;
 
+import fr.louisetom.profilsearch.mail.MailService;
 import fr.louisetom.profilsearch.model.Invitation;
 import fr.louisetom.profilsearch.repository.InvitationRepository;
 import fr.louisetom.profilsearch.service.InvitationService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import javax.mail.MessagingException;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.List;
 
 @RestController
@@ -15,10 +19,13 @@ import java.util.List;
 public class InvitationController {
 
     private final InvitationService invitationService;
+    private final MailService mailService;
 
     @PostMapping("/create")
-    public Invitation createInvitation(@RequestBody Invitation invitation) {
-        return invitationService.createInvitation(invitation);
+    public Invitation createInvitation(@RequestBody Invitation invitation) throws MessagingException, IOException {
+        Invitation invit = invitationService.createInvitation(invitation);
+        mailService.sendMailInvitation(invit);
+        return invit;
     }
 
     @GetMapping("/all")
--- src/main/java/fr/louisetom/profilsearch/mail/MailService.java
+++ src/main/java/fr/louisetom/profilsearch/mail/MailService.java
@@ -1,9 +1,11 @@
 package fr.louisetom.profilsearch.mail;
 
 import fr.louisetom.profilsearch.model.Candidature;
+import fr.louisetom.profilsearch.model.Invitation;
 import io.github.cdimascio.dotenv.Dotenv;
 import lombok.experimental.Helper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
@@ -12,7 +14,12 @@ import org.springframework.stereotype.Service;
 import javax.mail.MessagingException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 
 @Service
 public class MailService {
@@ -41,6 +48,36 @@ public class MailService {
 
         this.javaMailSender.send(message);
 
+    }
+
+    public void sendMailInvitation(Invitation invitation) throws MessagingException, IOException {
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, MimeMessageHelper.MULTIPART_MODE_MIXED, "UTF-8");
+
+        helper.setFrom(new InternetAddress(dotenv.get("MAIL_USERNAME"), "ProfilSearch"));
+        helper.setTo(invitation.getEmail());
+        helper.setSubject("Invitation à devenir administrateur de ProfilSearch");
+
+        String contentId = "logoImage";
+        ByteArrayResource imageResource = new ByteArrayResource(Files.readAllBytes(Paths.get("src/main/resources/static/logo.png")));
+
+
+        String htmlMsg = "<h1>Devenez administrateur de ProfilSearch</h1>" +
+                "Vous avez été invité à devenir administrateur de ProfilSearch, cliquez sur le lien suivant pour vous inscrire : </p>"
+                + "<a href='http://localhost:3000/signup'> S'inscrire </a>"
+                + "<p>Si vous ne connaissez pas ProfilSearch, vous pouvez ignorer cet email.</p>"
+                + "<p>Cordialement,</p>"
+                + "<p>L'équipe ProfilSearch</p>"
+                +"<img src='cid:" + contentId + "' style='width: 400px;'/>";
+
+        helper.setText(htmlMsg, true);
+
+
+        System.out.println(imageResource.getFilename());
+        helper.addInline(contentId, imageResource, "image/png");
+
+
+        javaMailSender.send(message);
     }
 
 }
