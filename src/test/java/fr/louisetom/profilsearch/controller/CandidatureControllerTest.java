2baec64ff8d0e24a8860ed2ac55890cf3edb20a1
Cegeeenre <73913043+Cegeeenre@users.noreply.github.com>
2023-04-05 16:38:34 +0200

    Update CandidatureControllerTest.java

--- src/test/java/fr/louisetom/profilsearch/controller/CandidatureControllerTest.java
+++ src/test/java/fr/louisetom/profilsearch/controller/CandidatureControllerTest.java
@@ -11,26 +11,42 @@ import fr.louisetom.profilsearch.service.CandidatureService;
 import fr.louisetom.profilsearch.service.OffreService;
 import fr.louisetom.profilsearch.service.QuestionService;
 import fr.louisetom.profilsearch.service.ReponseService;
+import static org.hamcrest.Matchers.is;
+import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+import static org.mockito.Mockito.*;
+import static org.mockito.ArgumentMatchers.*;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import fr.louisetom.profilsearch.controller.OffreController;
+import fr.louisetom.profilsearch.model.Offre;
+import fr.louisetom.profilsearch.service.OffreService;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import java.util.*;
-
-import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
-import static org.springframework.http.RequestEntity.post;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 public class CandidatureControllerTest {
 
@@ -98,4 +114,109 @@ public class CandidatureControllerTest {
         verify(reponseService, times(2)).createReponse(any(Reponse.class));
     }
 
+    @Test
+    public void shouldReturnAllCandidatures() throws Exception {
+        Question question1 = new Question("Vos qualités ?");
+        question1.setId(3L);
+        Question question2 = new Question("Pourquoi voulez-vous travailler avec nous ?");
+        question2.setId(4L);
+        Set<Question> questions = new HashSet<>(Arrays.asList(question1, question2));
+
+        // Créer un objet Offre à utiliser pour la candidature
+        Offre offre = new Offre("Dev Java", new Date(), "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", "CDD", "Lyon", 3999, questions);
+        offre.setId(4L);
+
+        // Créer un objet Candidature à partir des données que vous avez fournies
+        Candidature candidature1 = new Candidature("Doe", "John", "john@doe.com", offre);
+        candidature1.setId(5L);
+        List<Reponse> reponses = new ArrayList<>();
+        reponses.add(new Reponse("Réponse à la question 1", question1, candidature1));
+        reponses.add(new Reponse("Réponse à la question 2", question2, candidature1));
+        candidature1.setReponses(reponses);
+
+        Candidature candidature2 = new Candidature("Daffy", "Duck", "daffy@duck.com", offre);
+        candidature2.setId(6L);
+        reponses = new ArrayList<>();
+        reponses.add(new Reponse("Réponse à la question 1", question1, candidature2));
+        reponses.add(new Reponse("Réponse à la question 2", question2, candidature2));
+        candidature2.setReponses(reponses);
+
+        when(candidatureService.getAllCandidature()).thenReturn(List.of(candidature1, candidature2));
+
+        mockMvc.perform(get("/profilsearch/candidature/getAll"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].name", is("Doe")))
+                .andExpect(jsonPath("$[0].fname", is("John")))
+                .andExpect(jsonPath("$[0].email", is("john@doe.com")))
+                .andExpect(jsonPath("$[1].name", is("Daffy")))
+                .andExpect(jsonPath("$[1].fname", is("Duck")))
+                .andExpect(jsonPath("$[1].email", is("daffy@duck.com")));
+    }
+
+    @Test
+    public void shouldReturnCandidatureById() throws Exception {
+        Question question1 = new Question("Vos qualités ?");
+        question1.setId(3L);
+        Question question2 = new Question("Pourquoi voulez-vous travailler avec nous ?");
+        question2.setId(4L);
+        Set<Question> questions = new HashSet<>(Arrays.asList(question1, question2));
+
+        // Créer un objet Offre à utiliser pour la candidature
+        Offre offre = new Offre("Dev Java", new Date(), "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", "CDD", "Lyon", 3999, questions);
+        offre.setId(4L);
+
+        // Créer un objet Candidature à partir des données que vous avez fournies
+        Candidature candidature1 = new Candidature("Doe", "John", "john@doe.com", offre);
+        candidature1.setId(5L);
+        List<Reponse> reponses = new ArrayList<>();
+        reponses.add(new Reponse("Réponse à la question 1", question1, candidature1));
+        reponses.add(new Reponse("Réponse à la question 2", question2, candidature1));
+        candidature1.setReponses(reponses);
+
+        when(candidatureService.getCandidatureById(anyLong())).thenReturn(candidature1);
+
+        mockMvc.perform(get("/profilsearch/candidature/5"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.name", is("Doe")));
+    }
+
+    @Test
+    public void shouldReturnCandidatureByOffreId() throws Exception {
+        Question question1 = new Question("Vos qualités ?");
+        question1.setId(3L);
+        Question question2 = new Question("Pourquoi voulez-vous travailler avec nous ?");
+        question2.setId(4L);
+        Set<Question> questions = new HashSet<>(Arrays.asList(question1, question2));
+
+        // Créer un objet Offre à utiliser pour la candidature
+        Offre offre = new Offre("Dev Java", new Date(), "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", "CDD", "Lyon", 3999, questions);
+        offre.setId(4L);
+
+        // Créer un objet Candidature à partir des données que vous avez fournies
+        Candidature candidature1 = new Candidature("Doe", "John", "john@doe.com", offre);
+        candidature1.setId(5L);
+        List<Reponse> reponses = new ArrayList<>();
+        reponses.add(new Reponse("Réponse à la question 1", question1, candidature1));
+        reponses.add(new Reponse("Réponse à la question 2", question2, candidature1));
+        candidature1.setReponses(reponses);
+
+        Candidature candidature2 = new Candidature("Daffy", "Duck", "daffy@duck.com", offre);
+        candidature2.setId(6L);
+        reponses = new ArrayList<>();
+        reponses.add(new Reponse("Réponse à la question 1", question1, candidature2));
+        reponses.add(new Reponse("Réponse à la question 2", question2, candidature2));
+        candidature2.setReponses(reponses);
+
+
+        when(candidatureService.getCandidaturesByOffre(anyLong())).thenReturn(List.of(candidature1, candidature2));
+
+        mockMvc.perform(get("/profilsearch/candidature/offre/4"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].name", is("Doe")))
+                .andExpect(jsonPath("$[0].fname", is("John")))
+                .andExpect(jsonPath("$[0].email", is("john@doe.com")))
+                .andExpect(jsonPath("$[1].name", is("Daffy")))
+                .andExpect(jsonPath("$[1].fname", is("Duck")))
+                .andExpect(jsonPath("$[1].email", is("daffy@duck.com")));
+    }
 }
